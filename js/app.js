0166fa1e9cbe55cb8f2c442cd729373f868b7975
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-09-03 15:39:01 -0400

    configures wdiAddressBook app routes for people index and show person

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 (function wdiAddressBook(){
   var app = angular.module('wdiAddressBook', ['ngRoute', 'ngMessages']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'peopleController',
+        controllerAs: 'peepCtrl',
+        templateUrl: 'app/views/people.html'
+      })
+      .when ('/people/:personId', {
+        controller: 'personController',
+        controllerAs: 'personCtrl',
+        templateUrl: 'app/views/person.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
 })();
